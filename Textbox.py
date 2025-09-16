a2bea41f60445d043fcaa91629d1443d2abedc2c
beginner-champ <reshmaprasad1030@gmail.com>
2025-09-16 05:38:27 +0530

    Delete Textbox.py

--- Textbox.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-root = tkinter.Tk()
-
-name_label = tkinter.Label(root, text="enter name")
-name_label.pack()
-name_textbox=tkinter.Entry(root)
-name_textbox.pack()
-
-root.mainloop()
