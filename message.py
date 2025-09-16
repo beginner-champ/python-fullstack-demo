c7b5638771492e07176f9b83a7c64e99b6d6b34b
beginner-champ <reshmaprasad1030@gmail.com>
2025-09-16 05:42:48 +0530

    Delete message.py

--- message.py
+++
@@ -1,6 +0,0 @@
-from tkinter import messagebox
-
-messagebox.showinfo("Info", "Operation completed.")
-messagebox.showwarning("Warning", "This might be dangerous.")
-messagebox.showerror("Error", "Something went wrong.")
-messagebox.askyesno("Confirm", "Are you sure?")
