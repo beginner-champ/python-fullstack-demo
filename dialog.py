50833f084cf3fc5c0762791911e25cfbffaaaccc
beginner-champ <reshmaprasad1030@gmail.com>
2025-09-16 05:41:31 +0530

    Delete dialog.py

--- dialog.py
+++
@@ -1,6 +0,0 @@
-from tkinter import filedialog
-
-file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
-print("Selected file:",file_path)
-save_path = filedialog.asksaveasfilename(defaultextension=".txt")
-print("Save as:",save_path)
