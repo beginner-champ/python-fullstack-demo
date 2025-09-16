1827269f47edf32a1279e068048e0b7fc0765c6b
beginner-champ <reshmaprasad1030@gmail.com>
2025-09-16 05:43:54 +0530

    Delete text.py

--- text.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-root=tk.Tk()
-label = tk.Label(root,text="Hello World!", font=("arial",14), fg="blue")
-label.pack()
-text = tk.Text(root,height=5, width=30)
-text.pack()
-data = text.get("1.0", tk.END)
-root.mainloop()
